dde8ed841b2a54bd69b13c6ba7c59cae2908dfed
Nicola Massarenti <nicola.massarenti@gmail.com>
2020-04-20 21:26:59 +0200

    Button to create a new faq and modified the view for the training page

---
+++ src/components/CreateFaqComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <div class="col-10"></div>
+    <div class="col-2">
+      <div class="button-container center-align-button">
+        <button type="button" class="btn btn-success" v-on:click="createNewFaq()">
+          <i class="fas fa-plus"></i> Create new FAQ
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateFaqComponent",
+  data() {
+    return {};
+  },
+  methods: {
+    createNewFaq(){
+      this.$eventHub.$emit("createNewFaq");
+    }
+  }
+};
+</script>
---
+++ src/views/TrainingView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-fluid training-view">
+    <div class="row">
+      <p></p>
+    </div>
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col-10">
+        <div class="row">
+          <p></p>
+        </div>
+        <div class="row">
+          <div class="col-12 title-view">Training</div>
+        </div>
+        <div class="row">
+          <p></p>
+        </div>
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <div class="row">
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "TrainingView",
+  components: {
+  },
+  data() {
+    return {
+        
+    };
+  },
+  methods: {}
+};
+</script>
