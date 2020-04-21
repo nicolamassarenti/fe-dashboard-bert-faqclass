acf6d01d72574f1ab8d5c9152df73bc371a43e83
Nicola Massarenti <nicola.massarenti@gmail.com>
2020-04-21 22:26:14 +0200

    Modified the color of the button to create a new FAQ

--- src/components/CreateFaqComponent.vue
+++ src/components/CreateFaqComponent.vue
@@ -3,7 +3,7 @@
     <div class="col-10"></div>
     <div class="col-2">
       <div class="button-container center-align-button">
-        <button type="button" class="btn btn-success" v-on:click="createNewFaq()">
+        <button type="button" class="btn btn-light" v-on:click="createNewFaq()">
           <i class="fas fa-plus"></i> Create new FAQ
         </button>
       </div>
