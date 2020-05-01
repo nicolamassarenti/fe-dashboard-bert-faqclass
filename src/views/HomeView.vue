c83f56210836b52089e5ab7834702fed585e329d
NicolaMassarenti <nicola.massarenti@gmail.com>
2020-05-01 19:49:47 +0200

    First implementation of the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,42 @@
 		<div class="row"><p></p></div>
 		<div class="row">
 			<div class="col-1"></div>
-			<div class="col-10">HOME</div>
+			<div class="col-10 d-flex justify-content-center">
+				<h2>Welcome in the admin page</h2>
+			</div>
 			<div class="col-1"></div>
 		</div>
-		<div class="row">
+		<div class="row"  style="height: 100px">
 			<p></p>
+		</div>
+		<div class="row">
+			<div class="col-1"></div>
+			<div class="col-10">
+				<div class="row">
+					<div class="col-5">
+						<div class="card w-100" style="width: 18rem;">
+							<div class="card-body">
+								<h5 class="card-title">Knowledge Base</h5>
+								<h6 class="card-subtitle mb-2 text-muted">KB</h6>
+								<p class="card-text">Set up and manage the faqs and the examples. Make the chatbot to learn.</p>
+								<router-link to="/kb">Go to the Knowledge base panel</router-link>
+							</div>
+						</div>
+					</div>
+					<div class="col-2"></div>
+					<div class="col-5">
+						<div class="card w-100" style="width: 18rem;">
+							<div class="card-body">
+								<h5 class="card-title">Training Panel</h5>
+								<h6 class="card-subtitle mb-2 text-muted">Training status</h6>
+								<p class="card-text">Control the status of the training and read the logs.</p>
+								<router-link to="/training">Go to the training panel</router-link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-1"></div>
 		</div>
 	</div>
 </template>
