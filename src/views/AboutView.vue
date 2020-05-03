3a81293e384714bd5155487d2b312b790c844048
NicolaMassarenti <nicola.massarenti@gmail.com>
2020-05-03 19:32:54 +0200

    Added my pic in the about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,36 @@
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
+	<div class="container-fluid">
+		<div class="row"><p></p></div>
+		<div class="row">
+			<div class="col-1"></div>
+			<div class="col-10 d-flex justify-content-center">
+				<h2>About</h2>
+			</div>
+			<div class="col-1"></div>
+		</div>
+		<div class="row"  style="height: 100px">
+			<p></p>
+		</div>
+		<div class="row">
+			<div class="col-1"></div>
+			<div class="col-4">
+				<div class="card mb-3">
+					<img class="card-img-top" src="../assets/NicolaMassarenti.jpg" alt="Nicola Massarenti">
+					<div class="card-body">
+						<h5 class="card-title">Who I am</h5>
+						<p class="card-text">I'm Nicola Massarenti a Machine Learning Engineer.</p>
+						<p class="card-text">You can check out my references here:</p>
+						<div class="justify-content-around">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-2"></div>
+			<div class="col-4">
+			</div>
+			<div class="col-1"></div>
+		</div>
 	</div>
 </template>
 
