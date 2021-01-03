617f5086f524089d7d983091a6cafa3df4147d19
nicolamassarenti <nicola.massarenti@gmail.com>
2021-01-03 20:48:12 +0100

    Modified function for update and delete keywords

--- package/src/components/KeywordComponent.vue
+++ package/src/components/KeywordComponent.vue
@@ -1,82 +1,82 @@
 <template>
-  <div v-if="id">
-    <div class="card faq-card">
-      <div class="container-fluid">
-        <div class="row">
-          <div v-if="!edit" class="col-7 card-body">
-            <span>{{ keyword }}</span>
-          </div>
-          <div v-else class="col-7 card-body">
-            <input
-              type="text"
-              class="form-control"
-              aria-label="Default"
-              aria-describedby="inputGroup-sizing-default"
-              v-model="keyword"
-            />
-          </div>
-          <div class="col-5">
-            <div class="main-question-edit-container">
-              <button :class="btn_classes[edit]" v-on:click="modifySave()">
-                <i :class="font_awesome_classes[edit]"></i>
-              </button>
-              <button
-                class="btn btn-outline-danger"
-                v-on:click="deleteKeyword()"
-              >
-                <i class="fas fa-trash"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div v-if="id">
+		<div class="card faq-card">
+			<div class="container-fluid">
+				<div class="row">
+					<div v-if="!edit" class="col-7 card-body">
+						<span>{{ keyword }}</span>
+					</div>
+					<div v-else class="col-7 card-body">
+						<input
+							type="text"
+							class="form-control"
+							aria-label="Default"
+							aria-describedby="inputGroup-sizing-default"
+							v-model="keyword"
+						/>
+					</div>
+					<div class="col-5">
+						<div class="main-question-edit-container">
+							<button :class="btn_classes[edit]" v-on:click="modifySave()">
+								<i :class="font_awesome_classes[edit]"></i>
+							</button>
+							<button
+								class="btn btn-outline-danger"
+								v-on:click="deleteKeyword()"
+							>
+								<i class="fas fa-trash"></i>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "KeywordComponent",
-  components: {},
-  data() {
-    return {
-      edit: false,
-      font_awesome_classes: {
-        true: "fas fa-check",
-        false: "fas fa-edit",
-      },
-      btn_classes: {
-        true: "btn btn-outline-success",
-        false: "btn btn-outline-warning",
-      },
-    };
-  },
-  props: {
-    id: {
-      type: String,
-      required: false,
-    },
-    keyword: {
-      type: String,
-      required: false,
-    },
-  },
-  created() {},
-  methods: {
-    deleteKeyword() {
-      var data = {
-        id: this.id,
-      };
-      this.$eventHub.$emit("deleteKeyword", data);
-    },
-    modifySave() {
-      var data = {
-        id: this.id,
-        keyword: this.keyword,
-      };
-      this.$eventHub.$emit("edit", data);
-      this.edit = !this.edit;
-    },
-  },
+	name: "KeywordComponent",
+	components: {},
+	data() {
+		return {
+			edit: false,
+			font_awesome_classes: {
+				true: "fas fa-check",
+				false: "fas fa-edit",
+			},
+			btn_classes: {
+				true: "btn btn-outline-success",
+				false: "btn btn-outline-warning",
+			},
+		};
+	},
+	props: {
+		id: {
+			type: String,
+			required: false,
+		},
+		keyword: {
+			type: String,
+			required: false,
+		},
+	},
+	created() {},
+	methods: {
+		deleteKeyword() {
+			var data = {
+				id: this.id,
+			};
+			this.$eventHub.$emit("deleteKeyword", data);
+		},
+		modifySave() {
+			var data = {
+				id: this.id,
+				keyword: this.keyword,
+			};
+			this.$eventHub.$emit("editKeyword", data);
+			this.edit = !this.edit;
+		},
+	},
 };
 </script>
--- package/src/views/KeywordsView.vue
+++ package/src/views/KeywordsView.vue
@@ -1,59 +1,59 @@
 <template>
-	<div class="container-fluid kb-view">
-		<div class="row">
-			<p></p>
-		</div>
-		<div class="row">
-			<div class="col-1"></div>
-			<div class="col-10">
-				<div class="row">
-					<p></p>
-				</div>
-				<div class="row">
-					<div class="col-12 title-view">Keywords</div>
-				</div>
-				<div class="row">
-				<p></p>
-				</div>
-				<CreateKeywordComponent></CreateKeywordComponent>
-				<div class="row">
-				<p></p>
-				</div>
-				<div v-for="(item, index) in keywords" :key="index">
-					<div class="row">
-						<div class="col-3">
-							<KeywordComponent
-								:id="item[0].id"
-								:keyword="item[0].keyword"
-							></KeywordComponent>
-						</div>
-						<div class="col-3">
-							<KeywordComponent
-								:id="item[1].id"
-								:keyword="item[1].keyword"
-							></KeywordComponent>
-						</div>
-						<div class="col-3">
-							<KeywordComponent
-								:id="item[2].id"
-								:keyword="item[2].keyword"
-							></KeywordComponent>
-						</div>
-						<div class="col-3">
-							<KeywordComponent
-								:id="item[3].id"
-								:keyword="item[3].keyword"
-							></KeywordComponent>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-1"></div>
-		</div>
-		<div class="row">
-			<p></p>
-		</div>
+  <div class="container-fluid kb-view">
+	<div class="row">
+	  <p></p>
 	</div>
+	<div class="row">
+	  <div class="col-1"></div>
+	  <div class="col-10">
+		<div class="row">
+		  <p></p>
+		</div>
+		<div class="row">
+		  <div class="col-12 title-view">Keywords</div>
+		</div>
+		<div class="row">
+		  <p></p>
+		</div>
+		<CreateKeywordComponent></CreateKeywordComponent>
+		<div class="row">
+		  <p></p>
+		</div>
+		<div v-for="(item, index) in keywords" :key="index">
+		  <div class="row">
+			<div class="col-3">
+			  <KeywordComponent
+				:id="item[0].id"
+				:keyword="item[0].keyword"
+			  ></KeywordComponent>
+			</div>
+			<div class="col-3">
+			  <KeywordComponent
+				:id="item[1].id"
+				:keyword="item[1].keyword"
+			  ></KeywordComponent>
+			</div>
+			<div class="col-3">
+			  <KeywordComponent
+				:id="item[2].id"
+				:keyword="item[2].keyword"
+			  ></KeywordComponent>
+			</div>
+			<div class="col-3">
+			  <KeywordComponent
+				:id="item[3].id"
+				:keyword="item[3].keyword"
+			  ></KeywordComponent>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	  <div class="col-1"></div>
+	</div>
+	<div class="row">
+	  <p></p>
+	</div>
+  </div>
 </template>
 
 <script>
@@ -62,71 +62,107 @@ import KeywordComponent from "../components/KeywordComponent.vue";
 import CreateKeywordComponent from "../components/CreateKeywordComponent.vue";
 
 export default {
-	name: "KeywordsView",
-	components: {
-		KeywordComponent,
-		CreateKeywordComponent
+  name: "KeywordsView",
+  components: {
+	KeywordComponent,
+	CreateKeywordComponent,
+  },
+  data() {
+	return {
+	  urlKeywords: global.config.server + global.config.endpoints["keyword"],
+	  keywords: [
+		[
+		  {
+			id: "123",
+			keyword: "Pippo",
+		  },
+		  {
+			id: "456",
+			keyword: "Pluto",
+		  },
+		  {
+			id: "22",
+			keyword: "Alan",
+		  },
+		  {},
+		],
+		[
+		  {
+			id: "123",
+			keyword: "Pippo",
+		  },
+		  {
+			id: "456",
+			keyword: "Pluto",
+		  },
+		  {
+			id: "22",
+			keyword: "Alan",
+		  },
+		  {
+			id: "22",
+			keyword: "Asd",
+		  },
+		],
+	  ],
+	};
+  },
+  mounted() {
+	this.$eventHub.$on("createKeyword", this.createKeyword);
+	this.$eventHub.$on("deleteKeyword", this.deleteKeyword);
+	this.$eventHub.$on("editKeyword", this.editKeyword);
+  },
+  created() {
+	//this.keywords = this.getKeywords()
+  },
+  beforeDestroy() {
+	this.$eventHub.$off("createKeyword", this.createKeyword);
+	this.$eventHub.$off("deleteKeyword", this.deleteKeyword);
+	this.$eventHub.$off("editKeyword", this.editKeyword);
+  },
+  methods: {
+	async getKeywords() {
+	  let that = this;
+	  await axios
+		.get(that.urlKeywords, {
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		})
+		.then(function (response) {
+		  that.keywords = response.data.keywords;
+		})
+		.catch(function (error) {
+		  console.log(error);
+		});
 	},
-	data() {
-		return {
-		urlKeywords: global.config.server + global.config.endpoints["keyword"],
-		keywords: [
-			[
-				{
-					id: "123",
-					keyword: "Pippo"
-				},
-				{
-					id: "456",
-					keyword: "Pluto"
-				},
-				{
-					id: "22",
-					keyword: "Alan"
-				},
-				{
-					
-				}
-			],
-			[
-				{
-					id: "123",
-					keyword: "Pippo"
-				},
-				{
-					id: "456",
-					keyword: "Pluto"
-				},
-				{
-					id: "22",
-					keyword: "Alan"
-				},
-				{
-					id: "22",
-					keyword: "Asd"
-				}
-			],
-		]
-		};
+	createKeyword(data) {},
+	async deleteKeyword(data) {
+	  var parameters = {
+		params: data,
+	  };
+	  let that = this;
+	  await axios
+		.delete(that.urlKeywords, parameters)
+		.then(function () {
+		  conosle.log("A");
+		  //that.keywords = that.getKeywords();
+		})
+		.catch((err) => {
+		  console.log(err);
+		});
 	},
-	mounted() {
-		this.$eventHub.$on("createKeyword", this.createKeyword);
-		this.$eventHub.$on("deleteKeyword", this.deleteKeyword);
-		this.$eventHub.$on("keywordDetails", this.keywordDetails);
+	async editKeyword(data) {
+	  var parameters = {
+		params: data,
+	  };
+	  let that = this;
+	  await axios
+		.put(that.urlKeywords, null, parameters)
+		.catch((err) => {
+		  console.log(err);
+		});
 	},
-	created() {
-		//this.keywords = this.getKeywords()
-	},
-	beforeDestroy() {
-		this.$eventHub.$off("createKeyword", this.createKeyword);
-		this.$eventHub.$off("deleteKeyword", this.deleteKeyword);
-		this.$eventHub.$off("keywordDetails", this.keywordDetails);
-	},
-	methods: {
-		getKeywords(){},
-		createKeyword(data){},
-		deleteKeyword(data){},
-		keywordDetails(data){}
-	}
+  },
 };
 </script>
